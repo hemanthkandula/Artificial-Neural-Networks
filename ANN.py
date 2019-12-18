8edc3fb93aa84c0337c98b2a8200ea764deac4b5
Hemanth Kandula <hemanth.kandula@gmail.com>
2019-12-17 20:07:22 -0500

    back prop

--- ANN.py
+++ ANN.py
@@ -1,5 +1,5 @@
 import os
-from itertools import groupby
+from itertools import groupby, cycle
 
 import numpy as np
 import pandas as pd
@@ -23,31 +23,39 @@ def softmax(x):
 
 
 class FullyConnectedLayer:
-    def __init__(self, number_of_neurons, number_of_neurons_prev_layer,output_layer=False):
+    def __init__(self, number_of_neurons, number_of_neurons_prev_layer, output_layer=False, weights=None, biases=None):
 
         self.output_layer = output_layer
-        self.number_of_neurons =number_of_neurons
+        self.number_of_neurons = number_of_neurons
 
-        self._weights = np.random.rand( number_of_neurons,number_of_neurons_prev_layer)
-        self._biases = np.random.rand(number_of_neurons)
+        if weights is None and biases is None:
+            self._weights = np.random.rand(number_of_neurons, number_of_neurons_prev_layer)
+            self._biases = np.random.rand(number_of_neurons)
+
+        else:
+            self._weights = weights
+            self._biases = biases
 
     def output(self, inputs):
         # print(self._weights.shape, inputs.shape,self._biases.shape)
         # + np.dot(self._weights, inputs)
-        output_before_activation = np.dot(self._weights ,inputs)  + self._biases
-
+        output_before_activation = np.dot(self._weights, inputs) + self._biases
 
         if self.output_layer:
             layer_output = softmax(output_before_activation)
         else:
             layer_output = relu(output_before_activation)
 
-        return output_before_activation, layer_output
+        self.outputs = layer_output
+        self.outputs_derv = relu(output_before_activation)
 
+        self.output_before_activation = output_before_activation
+
+        return output_before_activation, layer_output
 
     def __str__(self):
         return "FullyConnectedLayer with " + str(self.number_of_neurons) \
-               +": weights shape = " + str(self._weights.shape)+ "  biases shape = " + str(self._biases.shape)
+               + ": weights shape = " + str(self._weights.shape) + "  biases shape = " + str(self._biases.shape)
 
 
 class NeuralNetwork:
@@ -57,26 +65,24 @@ class NeuralNetwork:
         self.output = output
         self.layers = []
 
+    # def get_weights_bias(self):
 
     def __str__(self):
         stri = ""
         for layer in self.layers:
-            stri+layer.__str__()+"\n"
+            stri + layer.__str__() + "\n"
             print(layer)
 
         return stri
         # stri = "Fully connected layer with  "+ num_neurons""
 
-
-
-
     def built(self):
         self.layers.append(FullyConnectedLayer(self.hidden_layers[0], self.inputs))  # First layer
 
         for prev_layer_neurons, number_of_neurons in zip(self.hidden_layers, self.hidden_layers[1:]):
-            self.layers.append(FullyConnectedLayer( number_of_neurons,prev_layer_neurons))  # hidden layers
+            self.layers.append(FullyConnectedLayer(number_of_neurons, prev_layer_neurons))  # hidden layers
 
-        self.layers.append(FullyConnectedLayer( self.output,self.hidden_layers[-1], output_layer=True))  # output layer
+        self.layers.append(FullyConnectedLayer(self.output, self.hidden_layers[-1], output_layer=True))  # output layer
 
     def forward_propagation(self, x):
         for layer in self.layers:
@@ -84,16 +90,82 @@ class NeuralNetwork:
 
         return before_activation, x
 
-    def back_propagation(self):
-        #         TODO :
+    def back_propagation(self, train_y):
+
+        # ouptut_error =
+        deltas = []
+        deltas.append((train_y - self.layers[-1].outputs) * self.layers[-1].outputs_derv)
+
+        for layer in reversed(self.layers[:-1]):
+
+            deltas.append(layer.outputs_derv * np.dot(layer.weights,deltas[-1]))
+
+            layer.weights += learning_rate * layer.outputs*deltas[-1]
+
+
+
+
 
         pass
 
-    def train(self,num_iterations, train_data,train_label,validation_data,validation_label):
-        pass
+    def train(self, num_iterations, train_data, train_label, validation_data, validation_label):
+        train_data_iterator, train_label_iterator = \
+            cycle(train_data), cycle(train_label)
+
+        weight = np.array([])
+        biases = np.array([])
+
+        valid_mse = []
+        current_mse = []
+
+        for itr in range(num_iterations):
+            train_x, train_y = next(train_data_iterator), next(train_label_iterator)
+
+            self.forward_propagation(train_x)
+
+            desired_output = one_hot_encoding(train_y)
+
+            self.back_propagation(desired_output)
+
+
+
+            valid_mse_cur =[]
+
+            for x, y in zip(validation_data, validation_label):
+                _, actual_output = self.forward_propagation(x)
+
+                desired_output = one_hot_encoding(y)
+                mse = self.mse(desired_output, actual_output)
+                valid_mse_cur.append(mse)
+
+            valid_mse = sum(valid_mse_cur)
+
+            valid_mse_cur.append(valid_mse)
+
+
+
+            # if len(current_mse) < 5:
+            #     current_mse.append(valid_mse)
+            # else:
+            #     if valid_mse > np.max(current_mse):
+            #         print(" STOP training")
+
+
+
+
+    def mse(self, d, o):
+
+        return np.sum((d - o) ** 2) / len(d)
 
     def test(self, test_data, test_label):
         pass
+
+
+def one_hot_encoding(x):
+    label = np.zeros(len(classes))
+    label[(x - 1)] = 1
+
+    return label
 
 
 if __name__ == '__main__':
@@ -132,38 +204,34 @@ if __name__ == '__main__':
     validation = np.array(validation, dtype=float)
     test = np.array(test, dtype=float)
 
+    x_train = train[:, :-1]
+    x_validation = validation[:, :-1]
+    x_test = test[:, :-1]
 
-
-
-
-
-    x_train = train[:,:-1]
-    x_validation = validation[:,:-1]
-    x_test = test[:,:-1]
-
-    y_train = train[:,-1]
-    y_validation = validation[:,-1]
-    y_test = test[:,-1]
-
+    y_train = train[:, -1]
+    y_validation = validation[:, -1]
+    y_test = test[:, -1]
 
     # train validation test split
 
     input_features = 4
     output_feature = 3
 
+    learning_rate = 0.1
+
+
     neural_network = NeuralNetwork(inputs=input_features, hidden_layers=[5, 5, 5], output=output_feature)
     neural_network.built()
 
     number_of_iterations = 1000
 
-
     print(neural_network.forward_propagation(x_train[0]))
 
     print(neural_network)
     neural_network.train(num_iterations=number_of_iterations,
-                         train_data=x_train,train_label=y_train,
-                         validation_data=x_validation,validation_label=y_validation)
-
-    neural_network.test(
-                         test_data=x_train,test_label=y_train,
-                         )
+                         train_data=x_train, train_label=y_train,
+                         validation_data=x_validation, validation_label=y_validation)
+    #
+    # neural_network.test(
+    #     test_data=x_train, test_label=y_train,
+    # )
